5fd848ff3d07e6695a84609b12c72f0880137ad1
f1shl3gs <fishlegs.engerman@gmail.com>
2025-09-20 01:34:59 +0800

    fix warning

--- conformance/tests/proto3/data_types.rs
+++ conformance/tests/proto3/data_types.rs
@@ -1,6 +1,4 @@
-use std::collections::HashMap;
-
-use ::prust::{Deserialize, sizeof_len};
+use ::prust::{Deserialize};
 
 mod prust {
     #![allow(dead_code, unused_imports)]
@@ -44,21 +42,6 @@ fn decode() {
     let out = prust::FooMessage::decode(input).unwrap();
     println!("out: {:?}", out.f_map);
     // println!("{}", out.encoded_len());
-}
-
-pub fn sizeof_varint(v: u64) -> usize {
-    match v {
-        0x0..=0x7F => 1,
-        0x80..=0x3FFF => 2,
-        0x4000..=0x1FFFFF => 3,
-        0x200000..=0xFFFFFFF => 4,
-        0x10000000..=0x7FFFFFFFF => 5,
-        0x0800000000..=0x3FFFFFFFFFF => 6,
-        0x040000000000..=0x1FFFFFFFFFFFF => 7,
-        0x02000000000000..=0xFFFFFFFFFFFFFF => 8,
-        0x0100000000000000..=0x7FFFFFFFFFFFFFFF => 9,
-        _ => 10,
-    }
 }
 
 // prost will not serialize map key or value if they are default value
