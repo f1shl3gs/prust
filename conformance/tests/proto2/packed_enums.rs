c66e5f79cf5d48e9aa5e36ff9dfab08aa9fc8033
f1shl3gs <fishlegs.engerman@gmail.com>
2025-10-06 23:27:13 +0800

    fmt

--- conformance/tests/proto2/packed_enums.rs
+++ conformance/tests/proto2/packed_enums.rs
@@ -10,60 +10,38 @@ conformance::fuzz!(Data);
 
 #[test]
 fn manually() {
-    use self::prust::{Data, State, LargeState};
+    use self::prust::{Data, LargeState, State};
     use ::prust::Deserialize;
 
     for (data, expected) in [
         (
-            [
-                1 << 3 | 0,
-                1,
-                1 << 3 | 0,
-                0,
-            ].as_slice(),
+            [1 << 3 | 0, 1, 1 << 3 | 0, 0].as_slice(),
             Data {
                 non_packed: vec![State::On, State::Off],
                 ..Default::default()
-            }
+            },
         ),
         (
-            [
-                2 << 3 | 2,
-                2,
-                1,
-                0
-            ].as_slice(),
+            [2 << 3 | 2, 2, 1, 0].as_slice(),
             Data {
                 packed: vec![State::On, State::Off],
                 ..Default::default()
-            }
+            },
         ),
         (
-            [
-                3 << 3 | 0,
-                128,
-                1,
-                3 << 3 | 0,
-                0,
-            ].as_slice(),
+            [3 << 3 | 0, 128, 1, 3 << 3 | 0, 0].as_slice(),
             Data {
                 large_non_packed: vec![LargeState::Yes, LargeState::No],
                 ..Default::default()
-            }
+            },
         ),
         (
-            [
-                4 << 3 | 2,
-                3,
-                128,
-                1,
-                0
-            ].as_slice(),
+            [4 << 3 | 2, 3, 128, 1, 0].as_slice(),
             Data {
                 large_packed: vec![LargeState::Yes, LargeState::No],
                 ..Default::default()
-            }
-        )
+            },
+        ),
     ] {
         let got = Data::decode(data).unwrap();
         assert_eq!(got, expected);
